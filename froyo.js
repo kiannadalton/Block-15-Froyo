cffe2ba0a42be8ac69a8604fd9b6341dc0da2729
kiannadalton <kiannadalton@aol.com>
2025-07-29 12:50:23 -0400

    The code is able to take a string, split at the comma, and move items into an Object called order.

---
+++ froyo.js
@@ -0,0 +1,41 @@
+// order is vanilla, vanilla, vanilla, strawberry, coffee, coffee
+// Ending output should look like the below, or something similar:
+// const order = {
+//     vanilla: 3,
+//     strawberry: 1,
+//     coffee: 2
+// }
+
+// Will need to loop through information
+
+// separate into an array
+// create for loop
+// create if statement. if they have a name in the list, add 1. if else, create new name in list
+// to add key: order[key] +=
+// to add new key: order[key] = 1;
+
+// Prompts user to input order
+let userOrderInput = prompt(
+  "Please type in your order, flavors separated by commas:",
+  ""
+);
+
+let froyoOrderArray = userOrderInput.split(",");
+console.log(froyoOrderArray);
+
+let order = {};
+function organizeOrder(inputOrder) {
+  for (let i = 0; i < inputOrder.length; i++) {
+    let currentFlavor = inputOrder[i];
+    if (order[currentFlavor]) {
+      order[currentFlavor] += 1;
+    } else {
+      order[currentFlavor] = 1;
+    }
+  }
+}
+
+organizeOrder(froyoOrderArray);
+
+console.log("This is console logging the order.");
+console.log(order);
